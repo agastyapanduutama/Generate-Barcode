9f09507413e4605bc7c6de51d57ddccb90bffca4
agastyapanduutama <agastypandu@gmail.com>
2022-06-19 12:24:23 +0700

    simple app for generate barcode

---
+++ index.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+</head>
+
+<body>
+    <div class="container mt-5">
+        <div class="container-fluid">
+            <div class="card text-center">
+                <div class="card-header">
+                    Generate Barcode
+                </div>
+                <center>
+                    <form action="" method="POST">
+                        <div class="card-body">
+                            <h5 class="card-title">Masukan Kode</h5>
+                            <div class="col-4">
+                                <input type="number" name="kode" class="form-control" placeholder="981723912">
+                            </div>
+                            <input type="submit" name="generate" id="btn_submit" class="btn btn-primary mt-2" value="Generate Code">
+                        </div>
+                    </form>
+                </center>
+            </div>
+        </div>
+    </div>
+
+    <div class="container mt-5">
+        <div class="container-fluid">
+            <div class="card text-center">
+                <div class="card-header">
+                    Barcode
+                </div>
+                <?php
+                    if (isset($_POST['generate'])){
+                        /*create folder*/
+                        $tempdir="img-barcode/";
+                        if (!file_exists($tempdir))
+                        mkdir($tempdir, 0755);
+                        $target_path=$tempdir . $_POST['kode'] . ".png";
+
+                        /*using server online
+                        $protocol=stripos($_SERVER['SERVER_PROTOCOL'], 'https') === 0 ? 'https://' : 'http://';
+                        $fileImage=$protocol . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . "/php-barcode/barcode.php?text=" . $_POST['kode_barang'] . "&codetype=code128&print=true&size=55";
+                        */
+                        /*using server localhost*/
+                        $fileImage="http://localhost".dirname($_SERVER['PHP_SELF']) . "/barcode.php?text=" . $_POST['kode'] . "&codetype=code128&print=true&size=55";
+
+                        /*get content from url*/
+                        $content=file_get_contents($fileImage);
+
+                        /*save file */
+                        file_put_contents($target_path, $content);
+
+                        echo "
+                        <p class='result'>Result :</p>
+                        <p><img src='barcode.php?text=" . $_POST['kode'] . "&codetype=code39&print=true&size=55' /></p>
+                        ";
+                    }
+                    ?>
+            </div>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
